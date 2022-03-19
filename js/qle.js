3fdbf81bbb7747a85116d172ca7cd3ad55d9c766
joric <joric@users.noreply.github.com>
2022-03-19 20:54:10 +0500

    updated files

--- js/qle.js
+++ js/qle.js
@@ -365,7 +365,7 @@ function render_raw(ctrl, data) {
   var fw = 8;
   var fh = 8;
   var w = parseInt($('#iw').val());
-  w = Math.max(8, (w<<3)>>3);
+  w = Math.min(512, Math.max(8, (w>>3)<<3));
   var cols = ~~(w / fw);
   var h = ~~(data.length / cols);
   let total = ~~(font.length / fw);
